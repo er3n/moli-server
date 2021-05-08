76f72bfcc9a2352b92a5ddd7f5fb093e93aac987
eren.ozturk <eren.ozturk@obss.com.tr>
2021-05-08 12:22:25 +0300

    invalid test

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -18,5 +18,9 @@ describe('AppController', () => {
     it('should return "Hello World!"', () => {
       expect(appController.getHello()).toBe('Hello World!');
     });
+
+    it('should return false result', () => {
+      expect(appController.getHello()).toBe('Hello World2!');
+    });
   });
 });
