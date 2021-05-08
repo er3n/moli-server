bd73d74591e560d3111aff182cae94e13079b69a
eren.ozturk <eren.ozturk@obss.com.tr>
2021-05-08 15:01:22 +0300

    jwt authentication and local authentication added

---
+++ src/auth/auth.service.spec.ts
@@ -0,0 +1,50 @@
+import { JwtService } from '@nestjs/jwt';
+import { Test, TestingModule } from '@nestjs/testing';
+import { UsersService } from '../users/users.service';
+import { AuthService } from './auth.service';
+
+describe('AuthService', () => {
+  let service: AuthService;
+  let usersService: UsersService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [AuthService, UsersService, JwtService],
+    }).compile();
+
+    service = module.get<AuthService>(AuthService);
+    usersService = module.get<UsersService>(UsersService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  it('when using local authentication if username and password correct validation should success', async () => {
+    jest.spyOn(usersService, 'findOne').mockImplementation(async () => ({
+      id: 1,
+      username: 'john',
+      password: 'password',
+    }));
+    const user = await service.validateUser('john', 'password');
+    expect(!!user).toBeTruthy();
+  });
+
+  it('when using local authentication if username not exists validation should fail', async () => {
+    jest
+      .spyOn(usersService, 'findOne')
+      .mockImplementation(async () => undefined);
+    const user = await service.validateUser('john', 'password');
+    expect(!!user).toBeFalsy();
+  });
+
+  it('when using local authentication if password wrong validation should fail', async () => {
+    jest.spyOn(usersService, 'findOne').mockImplementation(async () => ({
+      id: 1,
+      username: 'john',
+      password: 'password',
+    }));
+    const user = await service.validateUser('john', 'invalidpassword');
+    expect(!!user).toBeFalsy();
+  });
+});
